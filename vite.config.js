1ce0dd4be2eb57660045e459151469c0ead72826
Pal Roland <palroland2004@gmail.com>
2026-02-26 13:28:46 +0200

    Add public assets and update config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
   base: '/portfolio/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
